681964a7ac239b74407d33c44283400f3a1579e1
sr <joseph@wiznet.io>
2020-07-10 11:05:31 +0900

    Update email test with gmail account and hangul contents

--- test-email.py
+++ test-email.py
@@ -6,42 +6,35 @@ from email.message import EmailMessage
 from email.mime.text import MIMEText
 
 textfile = 'README.md'
-me1 = 'simryang@hotmail.com'
-me = 'simryangdanieldiary@gmail.com'
+me = 'kodico.wiznet2020@gmail.com'
 you = 'joseph@wiznet.io'
 HOTMAIL = 'smtp.live.com'
 GMAIL = 'smtp.gmail.com'
 
 smtpsvr = GMAIL
 
-smtp = smtplib.SMTP(smtpsvr, 587)
-smtp.ehlo()     # say Hello
-smtp.starttls() # TLS 사용시 필요
-smtp.login(me, 'password')
-
-msg = MIMEText('hi sr')
-msg['Subject'] = 'test from python'
-msg['To'] = you
-
-print(me, you, msg.as_string())
-smtp.sendmail(me, you, msg.as_string())
-
-smtp.quit()
-'''
-# Open the plain text file whose name is in textfile for reading.
-with open(textfile) as fp:
-    # Create a text/plain message
-    msg = EmailMessage()
-    msg.set_content(fp.read())
-
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = f'The contents of {textfile}'
-msg['From'] = me
-msg['To'] = you
-
-# Send the message via our own SMTP server.
-s = smtplib.SMTP('smtp.gmail.com', 465)
-s.send_message(msg)
-s.quit()
-'''
+print('email: Connecting server...')
+with smtplib.SMTP(smtpsvr, 587) as smtp:
+    try:
+        smtplib.SMTP()
+        print(f'\tSay hello to {smtpsvr}')
+        smtp.ehlo()     # say Hello
+        print(f'\tStarting TLS...')
+        smtp.starttls() # TLS 사용시 필요
+        print('\tTries to log in...')
+        smtp.login(me, '0vud0v1!')
+    except smtplib.SMTPException as e:
+        print (f"Error: unable to connect to email server {smtpsvr}:{e}")
+        pass
+    except Exception:
+        print(f"Unknown error: {e}")
+    print ('\tCreating email content...')
+    msg = MIMEText('안녕하세요 sr 입니다. 파이썬 모듈에서 메일을 보내봅니다.'.encode('utf-8'), _charset='UTF-8')
+    msg['Subject'] = 'test from python with Hangul'
+    msg['To'] = you
+    try:
+        smtp.sendmail(me, you, msg.as_string())
+    except smtplib.SMTPException as e:
+        print(f"Error: failed to send message {msg.as_string()} to {smtpsvr}")
+    print ('\tSending email is done!')
+    smtp.quit()
