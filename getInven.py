eb991099a9189112d1dd6a6c2a2e8af6e203ed2e
Joseph Daniel sr <simryangdaniel@nate.com>
2021-06-13 21:41:07 +0900

    Get latest news from maple inven

---
+++ getInven.py
@@ -0,0 +1,38 @@
+# from urllib import request
+import requests
+from bs4 import BeautifulSoup
+from pandas import DataFrame
+import sqlite3
+import datetime
+
+
+### 1단계
+
+url = "http://maple.inven.co.kr"
+html_contents = requests.get(url).text
+
+
+### 2단계
+
+soup = BeautifulSoup(html_contents, "html5lib")
+data1 = soup.find_all("ul", class_="list_txt board0")
+
+save_title = []
+for title in data1[1].find_all("span", class_="subject-text"):
+    save_title.append(title.text)
+
+save_link = []
+for link in data1[1].find_all("a"):
+    save_link.append(link.get("href"))
+
+
+### 3단계
+
+now = datetime.datetime.now()
+nowStr = now.strftime("%Y-%m-%d %H:%M")
+
+raw_data = {"title": save_title, "link": save_link}
+df = DataFrame(raw_data)
+
+con = sqlite3.connect("hot_inven.db")
+df.to_sql("%s" % nowStr, con)
